560f15609429701585dd2fb8630f094550fd520e
Nicolas Sasso <n.sasso56@gmail.com>
2025-10-07 18:16:57 +0200

    Formatting main.py.

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-
 import logging
 import os
 import sys
@@ -8,19 +7,21 @@ from pathlib import Path
 from src import eumetsat
 
 # Basic Logging configuration
-LOG_FILE = Path(__file__).with_suffix('.log')
+LOG_FILE = Path(__file__).with_suffix(".log")
 script_name = os.path.basename(__file__)
-#logger = logging.getLogger(script_name)
+# logger = logging.getLogger(script_name)
 
 logging.basicConfig(
     level=logging.INFO,  # DEBUG, INFO, WARNING, ERROR, CRITICAL
     format="%(asctime)s [%(levelname)s] %(name)s.%(funcName)s:%(lineno)d - %(message)s",
     handlers=[
-        logging.FileHandler(LOG_FILE, encoding="utf-8", mode="w"),  # log dans un fichier
-        logging.StreamHandler(sys.stdout)  # log aussi dans la console
+        logging.FileHandler(
+            LOG_FILE, encoding="utf-8", mode="w"
+        ),  # log dans un fichier
+        logging.StreamHandler(sys.stdout),  # log aussi dans la console
     ],
     # crucial if VSCode or Ipython already touched the logging
-    force=True
+    force=True,
 )
 # Name of the logger :
 logger = logging.getLogger(Path(__file__).stem)
@@ -31,13 +32,14 @@ start_time = datetime.utcnow() - timedelta(hours=6)
 end_time = datetime.utcnow() - timedelta(hours=2)
 
 # Collection required :
-required_collection = "EO:EUM:DAT:0677" # FCI AllSky radiance Level2 product
-output_files = eumetsat.download_data(required_collection, start_time, end_time, last = True)
-
+required_collection = "EO:EUM:DAT:0677"  # FCI AllSky radiance Level2 product
+output_files = eumetsat.download_data(
+    required_collection, start_time, end_time, last=True
+)
 
 
 for file in output_files:
     eumetsat.plot(file, required_collection)
 
 
-logger.info('End program.')
+logger.info("End program.")
