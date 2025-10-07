a88cb405a93e083d23b915729fdb97bffe6d1867
Nicolas Sasso <n.sasso56@gmail.com>
2025-10-07 16:54:58 +0200

    Creation of src/eumetsat.py functions.

--- main.py
+++ main.py
@@ -11,6 +11,8 @@ import matplotlib.pyplot as plt
 from netCDF4 import Dataset
 import h5py
 import hdf5plugin
+from src import eumetsat
+from datetime import datetime, timedelta
 
 from satpy import Scene
 from satpy import find_files_and_readers
@@ -23,7 +25,7 @@ script_name = os.path.basename(__file__)
 
 logging.basicConfig(
     level=logging.INFO,  # DEBUG, INFO, WARNING, ERROR, CRITICAL
-    format="%(asctime)s [%(levelname)s] %(name)s:%(lineno)d - %(message)s",
+    format="%(asctime)s [%(levelname)s] %(name)s.%(funcName)s:%(lineno)d - %(message)s",
     handlers=[
         logging.FileHandler(LOG_FILE, encoding="utf-8", mode="w"),  # log dans un fichier
         logging.StreamHandler(sys.stdout)  # log aussi dans la console
@@ -35,115 +37,18 @@ logging.basicConfig(
 logger = logging.getLogger(Path(__file__).stem)
 
 logger.info("Program launched.")
-# EUMETSAT authentification
-with open('inputs/id_EUMETSAT.json') as f:
-    d = json.load(f)
-    print(d)
-consumer_key = d["consumer"]
-consumer_secret = d["secret"]
-token = eumdac.AccessToken((consumer_key, consumer_secret))
 
-datastore = eumdac.DataStore(token)
-logger.info('Athentification succeed.')
-
+start_time = datetime.utcnow() - timedelta(hours=6)
+end_time = datetime.utcnow() - timedelta(hours=2)
 
 # Collection required :
 required_collection = "EO:EUM:DAT:0677" # FCI AllSky radiance Level2 product
-# required_collection =  "EO:EUM:DAT:MSG:HRSEVIRI" # 'EO:EUM:DAT:0677'
-logger.info(f'Get Collection {required_collection}')
-collection = datastore.get_collection(required_collection)
-
-# Research on a recent period (last day)
-results = collection.search(
-    dtstart="2025-09-20T00:00:00Z",
-    dtend="2025-09-20T12:00:00Z"
-)
-
-products = list(results)[:1]   # Select last product
-
-for product in products:
-    logger.info(f"Product : {product}")
-
-    target_dir = "./meteosat_data"
-    os.makedirs(target_dir, exist_ok=True)
+output_files = eumetsat.download_data(required_collection, start_time, end_time, last = True)
 
 
-    # Ouverture du produit (renvoie un flux HTTP)
-    # with product.open() as response:
-    #     outfile = os.path.join(target_dir, f"{product._id}.nc")
-    #     if os.path.exists(outfile) == False:
-    #         with open(outfile, "wb") as f:
-    #             f.write(response.read())
-    #         logger.info(f"Product saved : {outfile}")
-    #     else :
-    #         logger.info(f'Output file already exists : {outfile}')
-    for entry in product.entries:
 
-        if entry.endswith(".nc"):
-            logger.info("Download of NetCDF :", entry)
-            target_file = os.path.join(target_dir, entry)
-            if os.path.exists(target_file) == False:
-                with product.open(entry) as fsrc, open(target_file, "wb") as fdst:
-                    fdst.write(fsrc.read())
-                logger.info("→ File saved :", target_file)
-            else : 
-                logger.info(f"Target file already existent : {target_file}")
-    
+for file in output_files:
+    eumetsat.plot(file, required_collection)
 
-# ds = xr.open_dataarray(outfile, engine = "netcdf4")
-
-
-# with h5py.File(outfile, "r") as f:
-#     print("Groupes au niveau racine :", list(f.keys()))
-
-#     # Explorer récursivement
-#     def explore(name, obj):
-#         print(name, obj.shape if hasattr(obj, 'shape') else '')
-#     f.visititems(explore)
-
-
-# ds = Dataset(outfile, "r")
-# print(ds)
-# print(ds.variables.keys())
-
-
-# Find readers for sat files in Satpy :
-# files, readers = find_files_and_readers(
-#     base_dir="./meteosat_data", 
-#     reader=None,   # Satpy va deviner
-#     reader_kwargs={})
-
-# logger.info("Readers possibles :", readers)
-# logger.info("Fichiers trouvés :", files)
-
-# Afficher
-
-ds = xr.open_dataset(target_file)
-
-arr = ds["radiance_mean"].values
-logger.info("All NaN ? ->", np.isnan(arr).all())
-logger.info("Proportion of NaN ->", np.isnan(arr).sum() / arr.size)
-
-# lats = ds["latitude"].values
-# lons = ds["longitude"].values
-# val = ds["data"].values
-
-# logger.info(f"Lat shape : {lats.shape}")
-# logger.info(f"Lon shape : {lons.shape}")
-# logger.info(f"data shape : {val.shape}")
-
-
-vals = ds["radiance_mean"].values
-channel = 7
-fig, axes = plt.subplots(2, 3, figsize=(12, 8))
-for cat in range(6):
-    ax = axes.flat[cat]
-    img = vals[:, :, channel, cat]
-    im = ax.imshow(img, origin="lower")
-    ax.set_title(f"Category {cat}")
-    plt.colorbar(im, ax=ax, fraction=0.046, pad=0.04)
-
-plt.suptitle(f"FCI AllSkyRadiance - Channel {channel}")
-plt.show()
 
 logger.info('End program.')
---
+++ src/eumetsat.py
@@ -0,0 +1,135 @@
+import os, sys
+import json
+import numpy as np
+import xarray as xr
+import eumdac
+import matplotlib.pyplot as plt
+from datetime import datetime, timedelta
+import logging
+from pathlib import Path
+
+logger=logging.getLogger(__name__)
+
+def download_data(collection_id, start_time, end_time, last=False, output_file=None):
+
+    """
+    Download Eumetsat satellite Data via Eumdac API.
+    
+    Args :
+        - collection_id : ID of the required collection, available in EUMETSAT data store (string). 
+        - start_time : start ot the desired time period (datetime)
+        - end_time : end of the desired time period (datetime)
+        - last : specify as True if you want only the last file of the period (ie more recent) (boolean, default = False)
+        - output_file : specify if you want to save in a specific location (string, default = None)
+    
+    Returns output files path as a list of strings. 
+    """
+
+    # EUMETSAT authentification
+    logger.info('EUMETSAT authentification...')
+    project_root = Path(__file__).resolve().parent.parent
+    id_file = os.path.join(project_root, "inputs","id_EUMETSAT.json")
+    with open(id_file) as f:
+        d = json.load(f)
+        print(d)
+    consumer_key = d["consumer"]
+    consumer_secret = d["secret"]
+    token = eumdac.AccessToken((consumer_key, consumer_secret))
+    datastore = eumdac.DataStore(token)
+    logger.info('Athentification succeed.')
+
+    # Collection required :
+    # required_collection =  "EO:EUM:DAT:MSG:HRSEVIRI" # 'EO:EUM:DAT:0677'
+    logger.info(f'Get Collection {collection_id}')
+    collection = datastore.get_collection(collection_id)
+
+    # Research collection on a defined period :
+    logger.info(f"Download data for {collection_id} between {start_time} and {end_time}")
+    results = collection.search(
+        dtstart=start_time,
+        dtend=end_time
+    )
+
+    if last == True:
+        logger.info("last = True : Selection of only last product (more recent).")
+        products = list(results)[:1]   # Select only last product
+
+    outfiles = []
+
+    for product in products:
+        logger.info(f"Product : {product}")
+        target_dir = os.path.join(project_root, "data", collection_id)
+        os.makedirs(target_dir, exist_ok=True)
+
+        for entry in product.entries:
+            if entry.endswith(".nc"):
+                logger.info("Download of NetCDF :", entry)
+                target_file = os.path.join(target_dir, entry)
+                outfiles.append(target_file)
+
+                if (output_file != None):
+                    if len(products) > 1:
+                        logger.info("output_file is specified but products are mutliple : Conflict, return to default configuration.")
+                    else:
+                        target_file = output_file
+
+                if os.path.exists(target_file) == False:
+                    with product.open(entry) as fsrc, open(target_file, "wb") as fdst:
+                        fdst.write(fsrc.read())
+                    logger.info("→ File saved :", target_file)
+                else : 
+                    logger.info(f"Target file already existent : {target_file}")
+
+    return outfiles
+
+
+def plot(filename, collection_id, outfile = None, savefig =True):
+    '''
+    Plot the brut data contained in downloaded EUMETSAT .netcdf file.
+    Args :
+        - filename : name of the .netcdf file to treat.
+        - collection_id : Eumetsat collection ID from where data were extracted (mandatory).
+        - outfile : Specification of .png plot output (default = None, autogeneration of output filename). 
+        - savefig : Specify False for no output save (default = True).
+    '''
+    project_root = Path(__file__).resolve().parent.parent
+    if outfile == None:
+        outdir = os.path.join(project_root, "outputs", collection_id)
+        os.makedirs(outdir, exist_ok=True)
+        outfile = os.path.join(outdir, Path(filename).stem+'.png')
+        
+    ds = xr.open_dataset(filename)
+
+    arr = ds["radiance_mean"].values
+    logger.info(f"All NaN ? -> {np.isnan(arr).all()}")
+    logger.info(f"Proportion of NaN -> { np.isnan(arr).sum() / arr.size}")
+
+    # lats = ds["latitude"].values
+    # lons = ds["longitude"].values
+    # val = ds["data"].values
+
+    # logger.info(f"Lat shape : {lats.shape}")
+    # logger.info(f"Lon shape : {lons.shape}")
+    # logger.info(f"data shape : {val.shape}")
+
+    vals = ds["radiance_mean"].values
+    channel = 7
+    fig, axes = plt.subplots(2, 3, figsize=(12, 8))
+    for cat in range(6):
+        ax = axes.flat[cat]
+        img = vals[:, :, channel, cat]
+        im = ax.imshow(img, origin="lower")
+        ax.set_title(f"Category {cat}")
+        plt.colorbar(im, ax=ax, fraction=0.046, pad=0.04)
+
+    plt.suptitle(f"FCI AllSkyRadiance - Channel {channel}")
+    if savefig == True:
+        fig.tight_layout()
+        fig.savefig(outfile+'.png', format='png', bbox_inches='tight' ,dpi=300)
+    plt.show()
+
+
+
+
+
+    
